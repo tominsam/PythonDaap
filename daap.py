14fed86af2ae2c04b23867787116784616ba13f3
jason <jason@ab05c693-d3d1-0310-8e8b-e1346d90d6a5>
2006-04-20 19:34:52 +0000

    moved profiling code to its own script and added __all__ to daap.py

--- daap.py
+++ daap.py
@@ -12,12 +12,11 @@ import httplib, struct, sys
 import md5, md5daap
 import gzip
 import logging
+from cStringIO import StringIO
+
+__all__ = ['DAAPError', 'DAAPObject', 'DAAPClient', 'DAAPSession', 'DAAPDatabase', 'DAAPPlaylist', 'DAAPTrack']
 
 log = logging.getLogger('daap')
-
-# cStringIO is faster, but not subclassable
-#from StringIO import StringIO
-from cStringIO import StringIO
 
 # the itunes authentication hasher
 seed_v2 = []
@@ -619,7 +618,6 @@ if __name__ == '__main__':
 
             tracks[0].atom.printTree()
 
-
         finally:
             # this here, so we logout even if there's an error somewhere,
             # or itunes will eventually refuse more connections.
@@ -628,47 +626,4 @@ if __name__ == '__main__':
                 session.logout()
             except Exception: pass
 
-    def main_profile():
-        import hotshot
-
-        prof = hotshot.Profile('daap.prof')
-        connection  = DAAPClient()
-
-        # I'm new to this python thing. There's got to be a better idiom
-        # for this.
-        try: host = sys.argv[1]
-        except IndexError: host = "localhost"
-        try: port = sys.argv[2]
-        except IndexError: port = 3689
-
-        try:
-            # do everything in a big try, so we can disconnect at the end
-            
-            connection.connect( host, port )
-
-            # auth isn't supported yet. Just log in
-            session     = connection.login()
-
-            prof.start()
-
-            library = session.library()
-            tracks = library.tracks()
-
-        finally:
-            # this here, so we logout even if there's an error somewhere,
-            # or itunes will eventually refuse more connections.
-            try:
-                session.logout()
-            except Exception: pass
-
-            # save profiling data
-            prof.stop()
-            prof.close()
-
-        # load profile data and print out stats
-        import hotshot.stats
-        stats = hotshot.stats.load("daap.prof")
-        stats.print_stats()
-
-    #main()
-    main_profile()
+    main()
