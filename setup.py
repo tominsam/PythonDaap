d89cc500aeb90404147a221bad85b7a5075d3779
tomi <tomi@ab05c693-d3d1-0310-8e8b-e1346d90d6a5>
2007-02-12 17:48:51 +0000

    bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup, Extension
 
 setup (
   name = "PythonDaap",
-  version = "0.4",
+  version = "0.5",
   author = "Tom Insam",
   author_email = "tom@jerakeen.org",
-  url = "http://jerakeen.org/trac/wiki/PythonDaap",
+  url = "http://jerakeen.org/code/pythondaap",
   description = "a python daap client library",
   py_modules = ['daap'],
   ext_modules = [Extension('md5daap',sources=['md5module.c', 'md5c.c'])]
